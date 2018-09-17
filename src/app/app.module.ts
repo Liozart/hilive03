9af2e37dce1537857ea92e7689e21572a2231786
Chat <bobponay@gmail.com>
2018-09-17 22:39:21 +0200

    connect UI + sales page link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PromotionsPage } from "../pages/promotions/promotions";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PromotionsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PromotionsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/promotions/promotions.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-promotions',
+  templateUrl: 'promotions.html'
+})
+export class PromotionsPage {
+
+}
